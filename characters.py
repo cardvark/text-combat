ba7c99e072e2d81f2da142b13339b9f030d96ba2
James Wang <wang.james.j@gmail.com>
2025-11-08 11:32:26 -0800

    Initial character class creation.

--- characters.py
+++ characters.py
@@ -15,25 +15,40 @@ class Combatant(Environmental):
         super().__init__(name, description)
         self.__level = level
         self.__job = job
-        self.__max_hp = self.get_max_hp(level, self.__job)
-        self.__max_mp = self.get_max_mp(level, self.__job)
+        self.__max_hp = self.calc_max_hp(level, self.__job)
+        self.__max_mp = self.calc_max_mp(level, self.__job)
         self.__current_hp = self.__max_hp
         self.__current_mp = self.__max_mp
 
-    def get_max_hp(self, level, job):
+    def calc_max_hp(self, level, job):
         base_hp = 30
         hp_per_level = 5
         job_modifier = hp_modifiers[job]
-        return base_hp * hp_per_level * job_modifier
+        return int(base_hp * hp_per_level * job_modifier)
 
-    def get_max_mp(self, level, job):
+    def calc_max_mp(self, level, job):
         base_mp = 25
         mp_per_level = 3
         job_modifier = mp_modifiers[job]
-        return base_mp * mp_per_level * job_modifier
+        return int(base_mp * mp_per_level * job_modifier)
+
+    def get_max_hp(self):
+        return self.__max_hp
+
+    def get_max_mp(self):
+        return self.__max_mp
+
+    def get_current_hp(self):
+        return self.__current_hp
+    
+    def get_current_mp(self):
+        return self.__current_mp
+
+    def get_job(self):
+        return self.__job
 
     def __repr__(self):
-        return f"Combatant: {self.__name}\nDescription: {self.__description}\n\nJob: {self.__job.capitalize()}.\n\nHP: {self.__current_hp} / {self.__max_hp}\nMP: {self.__current_mp} / {self.__max_mp}"
+        return f"\n----------\nCombatant: {self.get_name()}\nDescription: {self.get_description()}\n\nJob: {self.get_job().capitalize()}\n\nHP: {self.get_current_hp()} / {self.get_max_hp()}\nMP: {self.get_current_mp()} / {self.get_max_mp()}\n----------\n"
 
     
 
@@ -44,7 +59,7 @@ jobs_list = [
     "thief",
 ]
 
-mp_modifiers = {
+hp_modifiers = {
     "fighter": 1.2,
     "mage": 0.8,
     "healer": 0.8,
