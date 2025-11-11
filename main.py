564f349db15b408b8bdecf224f61cb5486f3ce42
James Wang <wang.james.j@gmail.com>
2025-11-10 16:04:03 -0800

    Added basic turn by turn attacking.

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 from src.characters import Combatant
 from src.tools import *
 from src.player_functions import *
+from src.combat_sim import *
+import time
 
 def main():
     player1 = Combatant("Bob", "A vaguely nebbish creature.", 5, "fighter")
-    player2 = Combatant("Maria", "A comely young woman.", 4, "mage")
+    player2 = Combatant("ogre", "A foul, be-stenched beast looms three spans high.", 6, "fighter")
 
-    print(player1)
-    print(player2)
-
-    stick = Weapon("stick", "A sad, sickly little twig of a weapon.", "mace", "blunt", 5)
-    long_sword = Weapon("long sword", "A steel sword of some quality.", "sword", "slashing", 20)
+    stick = Weapon("stick", "A sad, sickly little twig of a weapon.", "mace", "blunt", 3)
+    long_sword = Weapon("long sword", "A steel sword of some quality.", "sword", "slashing", 10)
     small_heal = Potion("small healing potion", "A small vial of a wan red liquid.", "healing", 25)
     med_heal = Potion("large healing potion", "A vial of a bright red liquid.", "healing", 50)
 
@@ -22,21 +21,20 @@ def main():
     ]
 
     player1.equip_item(stick)
-    print(player1.main_equip.get_name())
-    old_item = player1.equip_item(long_sword)
-    print(f"Removed {old_item.get_name()}. Equipped {player1.main_equip.get_name()}.")
+    player2.equip_item(long_sword)
 
-    player1.take_damage(50)
-    print(player1.current_hp)
-    use_potion(inventory, player1, small_heal)
+    player_move = True
+    while (player1.is_conscious and player2.is_conscious):
+        print("\n------------\n")
+        if player_move:
+            player_turn(inventory, player1, player2)
+            player_move = False
+        else:
+            time.sleep(1)
+            enemy_turn(player1, player2)
+            player_move = True
+            time.sleep(1)
     
-    print(player1.current_hp)
-    print(inventory)
-
-    use_potion(inventory, player1, med_heal)
-        
-    print(player1.current_hp)
-    print(inventory)
 
 
 
---
+++ src/combat_sim.py
@@ -0,0 +1,117 @@
+from src.characters import Combatant
+from src.tools import *
+from src.player_functions import *
+import random
+
+def enemy_turn(player, enemy):
+    print(f"The {enemy.name} attacks!")
+    original_hp_perc = player.current_hp / player.max_hp
+    damage = enemy_attack(player, enemy)
+    if damage == None:
+        return
+
+    print(f"The {enemy.name} strikes you with {enemy.main_equip.name}, dealing {damage} damage.")
+
+    new_hp_perc = player.current_hp / player.max_hp
+    damage_flavor(original_hp_perc, new_hp_perc)
+
+def damage_flavor(original_hp_perc, new_hp_perc):
+    damage_delta = original_hp_perc - new_hp_perc
+
+    flavor = ""
+
+    if damage_delta > 0.25:
+        options = [
+            "You gasp with the shock of pain, clutching at the fresh wound in your side with a feeling of disbelief. ",
+            "Your head rings from the blow, and your enemy appears doubled in your vision for a long, lingering moment. ",
+        ]
+        flavor += random.choice(options)
+
+    if original_hp_perc > 0.2 and new_hp_perc <= 0.2:
+        flavor += "The world dims at the edges, and takes on a grainy aspect. The sharp pains have dulled, and some dim part of you recognizes that you are in deep trouble."
+    elif original_hp_perc > 0.5 and new_hp_perc <= 0.5:
+        flavor += "Your body flags under the onslaught, but you grip your weapon tighter, and face your opponent squarely, ready for more."
+
+    if flavor:
+        print(flavor)
+    
+    
+
+def player_turn(inventory, player, enemy):
+    print_status(player, enemy)
+
+    options = {
+        "1": "Attack",
+        "2": "Use an item",
+        "3": "Flee",
+    }
+
+    while True:
+        prompt = "Your options include: \n"
+        for k, v in sorted(options.items()):
+            prompt += f"[{k}] {v}\n"
+
+        prompt += ">> "
+        
+        player_choice = input(prompt)
+
+        if player_choice in options:
+            break
+        else:
+            print("Select one of the options.")
+
+    match player_choice:
+        case "1":
+            print(f"You attack the {enemy.name}.")
+            damage = player_attack(player, enemy)
+
+            if damage != None:
+                print(f"You hit your opponent squarely, dealing {damage} damage.")
+            
+        case "2":
+            # TODO 
+            print("working on this.")
+            pass
+        case "3":
+            print("There is no fleeing from this fight.")
+            pass
+        
+        
+def player_attack(player, enemy):
+    if not check_hit(player, enemy):
+        print("You miss your attack!")
+        return None
+    
+    return deal_damage(player, enemy)
+
+def enemy_attack(player, enemy):
+    if not check_hit(enemy, player):
+        print(f"The {enemy.name} misses their attack!")
+        return None
+    
+    return deal_damage(enemy, player)
+
+
+def print_status(player, enemy):
+    enemy_status_dict = {
+        0.9: "pristine and malevolent",
+        0.8: "barely scratched",
+        0.6: "bleeding, but hardly winded",
+        0.3: "tired, but determined",
+        0.15: "haggard, and panting heavily",
+        0.0: "on death's door",
+    }
+
+    enemy_hp_perc = enemy.current_hp / enemy.max_hp
+
+    enemy_status = "bland."
+
+    for k, v in sorted(enemy_status_dict.items(), reverse=True):
+        if k <= enemy_hp_perc:
+            enemy_status = v
+            break
+        
+
+    print(f"You have {player.current_hp} out of {player.max_hp} HPs.")
+    print(f"Your opponent appears {enemy_status}.")
+
